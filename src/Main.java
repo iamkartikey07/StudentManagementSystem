1adea0c7a57f28a6fa949224e906d4481ebe6726
iamkartikey07 <kartikeyawasthi69@gmail.com>
2026-06-23 14:23:50 +0530

    Link all classes in Main and display student, section, teacher and result data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args){
+        Teacher t = new Teacher("Mr. Ramesh" , 38, "Male" , "TCH20089" , "G");
+        Subject s = new Subject("BAS201","Maths","Mr. Ramesh");
+        t.addSubject(s);
+        Section se = new Section("D","10",t);
+        Student st = new Student("Parth",15,"Male","STU1197",43,"D",true);
+        se.addStudent(st);
+        st.enrollSubject(s);
+        Result r = new Result(69,2,s);
 
+
+        System.out.println(st.getName());
+        System.out.println(st.getRollNum());
+        System.out.println(st.getGender());
+        System.out.println(st.getId());
+        System.out.println(st.getRole());
+        System.out.println(st.getSection());
+        System.out.println(st.getStatus());
+        System.out.println(st.isFeesPaid());
+        System.out.println(st.isHosteler());
+        System.out.println(se.getGrade());
+        System.out.println(t.getName());
+        r.displayResult();
     }
 }
